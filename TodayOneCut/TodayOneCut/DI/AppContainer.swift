141313df416320eb531a0a2c2fced1f4dc38b07b
sin-jun-woo <46078573+sin-jun-woo@users.noreply.github.com>
2026-02-14 19:47:49 +0900

    AppContainer 구조 완성 (Swinject 추가 대기)
    - Swinject import 주석 추가
    - 전체 의존성 등록 구조 작성 (Phase 1, 2에서 구현 예정)
    - resolve 메서드 구조 추가

--- TodayOneCut/TodayOneCut/DI/AppContainer.swift
+++ TodayOneCut/TodayOneCut/DI/AppContainer.swift
@@ -6,20 +6,22 @@
 //
 
 import Foundation
-
-// TODO: Swinject 추가 후 구현
-// Swift Package Manager로 Swinject 추가 필요:
-// 1. Xcode에서 File > Add Package Dependencies...
-// 2. https://github.com/Swinject/Swinject.git 추가
-// 3. import Swinject 추가
+// TODO: Xcode에서 Swinject 추가 후 주석 해제
+// import Swinject
 
 /// 의존성 주입 컨테이너
 /// 
 /// Swinject를 사용하여 의존성을 관리합니다.
+/// 
+/// Swinject 추가 방법:
+/// 1. Xcode에서 File > Add Package Dependencies...
+/// 2. https://github.com/Swinject/Swinject.git 입력
+/// 3. Add Package 클릭
+/// 4. 위의 import Swinject 주석 해제
 class AppContainer {
     static let shared = AppContainer()
     
-    // TODO: Swinject Container 추가
+    // TODO: Swinject 추가 후 주석 해제
     // private let container = Container()
     
     private init() {
@@ -28,12 +30,22 @@ class AppContainer {
     
     private func setupDependencies() {
         // TODO: Swinject 추가 후 구현
-        // 
+        // Phase 1, 2에서 실제 구현할 때 아래 주석 해제
+        //
         // // Core Data Stack
         // container.register(CoreDataStack.self) { _ in
         //     CoreDataStack.shared
         // }.inObjectScope(.container)
-        // 
+        //
+        // // Mappers
+        // container.register(RecordMapper.self) { _ in
+        //     RecordMapper()
+        // }
+        //
+        // container.register(SettingsMapper.self) { _ in
+        //     SettingsMapper()
+        // }
+        //
         // // Repositories
         // container.register(RecordRepository.self) { r in
         //     RecordRepositoryImpl(
@@ -41,8 +53,38 @@ class AppContainer {
         //         recordMapper: r.resolve(RecordMapper.self)!
         //     )
         // }.inObjectScope(.container)
-        // 
-        // // Use Cases
+        //
+        // container.register(SettingsRepository.self) { r in
+        //     SettingsRepositoryImpl(
+        //         coreDataStack: r.resolve(CoreDataStack.self)!,
+        //         settingsMapper: r.resolve(SettingsMapper.self)!
+        //     )
+        // }.inObjectScope(.container)
+        //
+        // container.register(FileRepository.self) { _ in
+        //     FileRepositoryImpl()
+        // }.inObjectScope(.container)
+        //
+        // // Validation Use Cases
+        // container.register(ValidateDailyLimitUseCase.self) { r in
+        //     ValidateDailyLimitUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(ValidateUpdateLimitUseCase.self) { _ in
+        //     ValidateUpdateLimitUseCase()
+        // }
+        //
+        // container.register(ValidateDateUseCase.self) { _ in
+        //     ValidateDateUseCase()
+        // }
+        //
+        // container.register(ValidateRecordContentUseCase.self) { _ in
+        //     ValidateRecordContentUseCase()
+        // }
+        //
+        // // Record Use Cases
         // container.register(CreateRecordUseCase.self) { r in
         //     CreateRecordUseCase(
         //         recordRepository: r.resolve(RecordRepository.self)!,
@@ -51,7 +93,93 @@ class AppContainer {
         //         validateDate: r.resolve(ValidateDateUseCase.self)!
         //     )
         // }
-        // 
+        //
+        // container.register(GetTodayRecordUseCase.self) { r in
+        //     GetTodayRecordUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(GetAllRecordsUseCase.self) { r in
+        //     GetAllRecordsUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(GetRecordByIdUseCase.self) { r in
+        //     GetRecordByIdUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(UpdateRecordUseCase.self) { r in
+        //     UpdateRecordUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!,
+        //         fileRepository: r.resolve(FileRepository.self)!,
+        //         validateUpdateLimit: r.resolve(ValidateUpdateLimitUseCase.self)!
+        //     )
+        // }
+        //
+        // container.register(DeleteRecordUseCase.self) { r in
+        //     DeleteRecordUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!,
+        //         fileRepository: r.resolve(FileRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(SearchRecordsUseCase.self) { r in
+        //     SearchRecordsUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(CheckTodayRecordExistsUseCase.self) { r in
+        //     CheckTodayRecordExistsUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // // Calendar Use Cases
+        // container.register(GetMonthRecordsUseCase.self) { r in
+        //     GetMonthRecordsUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(GetRecordDatesUseCase.self) { r in
+        //     GetRecordDatesUseCase(
+        //         recordRepository: r.resolve(RecordRepository.self)!
+        //     )
+        // }
+        //
+        // // Settings Use Cases
+        // container.register(GetSettingsUseCase.self) { r in
+        //     GetSettingsUseCase(
+        //         settingsRepository: r.resolve(SettingsRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(UpdateLocationSettingUseCase.self) { r in
+        //     UpdateLocationSettingUseCase(
+        //         settingsRepository: r.resolve(SettingsRepository.self)!
+        //     )
+        // }
+        //
+        // container.register(UpdateThemeUseCase.self) { r in
+        //     UpdateThemeUseCase(
+        //         settingsRepository: r.resolve(SettingsRepository.self)!
+        //     )
+        // }
+        //
+        // // Location Use Cases
+        // container.register(GetCurrentLocationUseCase.self) { _ in
+        //     GetCurrentLocationUseCase()
+        // }
+        //
+        // container.register(ReverseGeocodeUseCase.self) { _ in
+        //     ReverseGeocodeUseCase()
+        // }
+        //
         // // ViewModels
         // container.register(HomeViewModel.self) { r in
         //     HomeViewModel(
@@ -61,7 +189,7 @@ class AppContainer {
         // }
     }
     
-    // TODO: Swinject 추가 후 구현
+    // TODO: Swinject 추가 후 주석 해제
     // func resolve<T>(_ type: T.Type) -> T? {
     //     return container.resolve(type)
     // }
